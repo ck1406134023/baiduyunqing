85b375f7b20049aa7574f63e34230a52f2fa2908
ck1406134023 <1406134023@qq.com>
2016-05-03 22:28:44 +0800

    第一次上传

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*
+var tiao=$(".xs .tiao");
+var login=$(".loginbox");
+console.log(tiao);
+console.log(login);
+console.log(1);
+tiao.onclick=function(){
+    login.style.display="block";
+    login.animate({height:"auto"});
+}*/
